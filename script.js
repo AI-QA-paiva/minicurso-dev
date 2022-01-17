65054571c772554db6d534cb0532deb88d00fa66
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-01-16 21:53:02 -0300

    Resolver formatação CSS e imagem interrogatica inicial

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-/*const imagem = document.getElementsByTagName('img');*/
-/*const imagem = document.querySelector('img');*/
-
 const imagens = document.querySelectorAll('img')
-/*alert(imagens[0].src)*/
-
 const botao = document.querySelector('button');
 
 const nomeDoPersonagem = document.querySelector('#nome');
@@ -72,3 +67,4 @@ pegarPersonagem = () => {
 }
 
 botao.onclick = pegarPersonagem;
+
